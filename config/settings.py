89cb9f87904355a8ff156913e9698873ac890e62
asliddin3101 <kamoliddinovasliddin90@gmail.com>
2021-11-09 20:57:51 +0300

    mikm

--- config/settings.py
+++ config/settings.py
@@ -78,6 +78,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
